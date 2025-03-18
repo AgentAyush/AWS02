e56f76484e4b91c5311e28d792116521b00aef92
Ayush Das <ayush_das@epam.com>
2025-03-18 14:35:40 +0530

    task04 again

--- task04/app/lambdas/sqs_handler/index.js
+++ task04/app/lambdas/sqs_handler/index.js
@@ -1,22 +1,9 @@
 exports.handler = async (event) => {
-    console.log("Received SQS event:", JSON.stringify(event, null, 2));
+  console.log("Received SQS Event:", JSON.stringify(event, null, 2));
 
-    try {
-        for (const record of event.Records) {
-            console.log("Processing message:", record.body);
+  for (const record of event.Records) {
+    console.log("Message Body:", record.body);
+  }
 
-            // Add your processing logic here
-        }
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message: "SQS messages processed successfully" }),
-        };
-    } catch (error) {
-        console.error("Error processing SQS message:", error);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: "Failed to process messages" }),
-        };
-    }
+  return { statusCode: 200, body: "Processed SQS Messages" };
 };
